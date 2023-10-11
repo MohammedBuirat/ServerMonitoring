0e15b39ded9246301c5b8d44fa96235505606212
Mohammed Buirat <1192896@student.birzeit.edu>
2023-10-11 14:22:56 +0300

    Creating consumerservice

---
+++ ConsumerService/Entities/ServerStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerService.Entities
+{
+    internal class ServerStatistics
+    {
+        public String ServerIdentifier { get; set; }
+        public double MemoryUsage { get; set; } // in MB
+        public double AvailableMemory { get; set; } // in MB
+        public double CpuUsage { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ ConsumerService/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace ConsumerService
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IConfiguration configuration = GetConfiguration();
+
+        }
+
+        static IConfiguration GetConfiguration()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+            return configuration;
+        }
+    }
+}
