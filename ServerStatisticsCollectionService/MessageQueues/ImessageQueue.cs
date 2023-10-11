510e33cda07246e95190d0972852a7c218655342
Mohammed Buirat <1192896@student.birzeit.edu>
2023-10-11 14:01:15 +0300

    Building Message Queue

---
+++ ServerStatisticsCollectionService/MessageQueues/ImessageQueue.cs
@@ -0,0 +1,15 @@
+﻿using ServerStatisticsCollectionService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatisticsCollectionService.MessageQueues
+{
+    public interface IMessageQueue
+    {
+        public void Publish(string serverStatisticsMessage);
+    }
+}
---
+++ ServerStatisticsCollectionService/MessageQueues/RabbitMQMessageQueue.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace ServerStatisticsCollectionService.MessageQueues
+{
+    public class RabbitMQMessageQueue : IMessageQueue
+    {
+        private readonly ConnectionFactory _factory;
+        private readonly string _queueName;
+
+        public RabbitMQMessageQueue(string hostName, string queueName)
+        {
+            _factory = new ConnectionFactory() { HostName = hostName };
+            _queueName = queueName;
+        }
+
+        public void Publish(string serverStatisticsMessage)
+        {
+            using (var connection = _factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                var body = Encoding.UTF8.GetBytes(serverStatisticsMessage);
+                channel.BasicPublish(exchange: "", routingKey: _queueName, basicProperties: null, body: body);
+            }
+        }
+    }
+}
