baef22fc649478698b6c056bdf0812d58adf1530
Mohammed Buirat <1192896@student.birzeit.edu>
2023-10-11 13:07:46 +0300

    Building Parser

---
+++ ServerStatisticsCollectionService/Parser/Parser.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using ServerStatisticsCollectionService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerStatisticsCollectionService.Parser
+{
+    public class Parser
+    {
+        public string ServerStatisticsToJson(ServerStatistics serverStatistics)
+        {
+            var serverStatisticsConfigSection = _configuration.GetSection("ServerStatisticsConfig");
+            string serverIdentifier = serverStatisticsConfigSection["ServerIdentifier"];
+            string serverStatisticsJson = JsonConvert.SerializeObject(serverStatistics);
+            var jsonData = new JObject();
+            jsonData[serverIdentifier] = JToken.Parse(serverStatisticsJson);
+            string jsonString = jsonData.ToString();
+            return jsonString;
+        }
+    }
+}
